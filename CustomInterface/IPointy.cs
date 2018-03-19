45aef0ea15680bf1710123a08012eae6e4ec74f4
Michal Krupinski <michal.krupinski@fuerogames.pl>
2018-03-19 18:36:45 +0100

    286

---
+++ CustomInterface/IPointy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    // This interface defines the behavior of "having points."
+    interface IPointy
+    {
+        // A read-write property in an interface would look like:
+        // retType PropName { get; set; }
+        //
+        // while a write-only property in an interface would be:
+        // retType PropName { set; }
+        byte Points { get; }
+    }
+}
